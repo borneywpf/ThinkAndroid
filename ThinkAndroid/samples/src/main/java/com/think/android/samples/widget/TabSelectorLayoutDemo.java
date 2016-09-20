32c75590e86e3ed0a6a8d6f3a9dda3bccd8e844b
borney <borneywpf@gmail.com>
2016-09-20 17:17:12 +0800

    编辑TabSelectorLayout

--- ThinkAndroid/samples/src/main/java/com/think/android/samples/widget/TabSelectorLayoutDemo.java
+++ ThinkAndroid/samples/src/main/java/com/think/android/samples/widget/TabSelectorLayoutDemo.java
@@ -1,18 +1,34 @@
 package com.think.android.samples.widget;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.think.android.samples.R;
 import com.think.android.widget.TabSelectorLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by borney on 9/19/16.
  */
 public class TabSelectorLayoutDemo extends AppCompatActivity {
     private TabSelectorLayout mTabSelectorLayout;
+    private ViewPager mViewPager;
+    private Map<Integer, MyFragment> mFragmentMap = new HashMap<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +37,71 @@ public class TabSelectorLayoutDemo extends AppCompatActivity {
         mTabSelectorLayout = (TabSelectorLayout) findViewById(R.id.tabselectorlayout);
 
         Resources resources = getResources();
-        mTabSelectorLayout.addTab(TabSelectorLayout.newTab().setDrawable(resources.getDrawable(R.drawable.tab_contact)).setPosition(0).setTitle("contact"));
-        mTabSelectorLayout.addTab(TabSelectorLayout.newTab().setDrawable(resources.getDrawable(R.drawable.tab_moment)).setPosition(1).setTitle("moment"));
-        mTabSelectorLayout.addTab(TabSelectorLayout.newTab().setDrawable(resources.getDrawable(R.drawable.tab_msg)).setPosition(2).setTitle("message"));
-        mTabSelectorLayout.addTab(TabSelectorLayout.newTab().setDrawable(resources.getDrawable(R.drawable.tab_person)).setPosition(3).setTitle("person"));
+        int pos = mTabSelectorLayout.addTab(TabSelectorLayout.newTab()
+                .setNormalDrawable(resources.getDrawable(R.drawable.ic_tab_contact))
+                .setSelectDrawable(resources.getDrawable(R.drawable.ic_tab_contact_select))
+                .setTitle("contact"));
+        mFragmentMap.put(pos, new MyFragment(pos));
+
+        pos = mTabSelectorLayout.addTab(TabSelectorLayout.newTab()
+                .setNormalDrawable(resources.getDrawable(R.drawable.ic_tab_moment))
+                .setSelectDrawable(resources.getDrawable(R.drawable.ic_tab_moment_select))
+                .setTitle("moment"));
+        mFragmentMap.put(pos, new MyFragment(pos));
+
+        pos = mTabSelectorLayout.addTab(TabSelectorLayout.newTab()
+                .setNormalDrawable(resources.getDrawable(R.drawable.ic_tab_msg))
+                .setSelectDrawable(resources.getDrawable(R.drawable.ic_tab_msg_select))
+                .setTitle("message"));
+        mFragmentMap.put(pos, new MyFragment(pos));
+
+        pos = mTabSelectorLayout.addTab(TabSelectorLayout.newTab()
+                .setNormalDrawable(resources.getDrawable(R.drawable.ic_tab_person))
+                .setSelectDrawable(resources.getDrawable(R.drawable.ic_tab_person_select))
+                .setTitle("person"));
+        mFragmentMap.put(pos, new MyFragment(pos));
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        mTabSelectorLayout.bindViewPager(mViewPager);
+    }
+
+
+    class FragmentAdapter extends FragmentPagerAdapter {
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentMap.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentMap.size();
+        }
+    }
+
+    class MyFragment extends Fragment {
+        private int position;
+
+        MyFragment(int position) {
+            this.position = position;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            Context context = container.getContext();
+            TextView textView = new TextView(context);
+            textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 36, context.getResources().getDisplayMetrics()));
+            textView.setText(String.valueOf(position));
+            return textView;
+        }
     }
 }
--- ThinkAndroid/thinkandroid/src/main/java/com/think/android/widget/TabSelectorLayout.java
+++ ThinkAndroid/thinkandroid/src/main/java/com/think/android/widget/TabSelectorLayout.java
@@ -1,24 +1,51 @@
 package com.think.android.widget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.StateListDrawable;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.think.android.R;
+
 /**
  * Created by borney on 9/19/16.
  */
 public class TabSelectorLayout extends ViewGroup {
     private static final String TAG = "TAGTabSelectorLayout";
+    private ViewPager viewPager;
+    private int textSize = 12;
+    private int drawablePadding = 10;
+    private int normalTextColor, selectTextColor;
+
+    private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.SimpleOnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            TabView cur = (TabView) getChildAt(position);
+            if (positionOffset > 0) {
+                cur.setTabOffSet(1 - positionOffset);
+                TabView next = (TabView) getChildAt(position + 1);
+                next.setTabOffSet(positionOffset);
+            } else {
+                cur.setTabOffSet(1 - positionOffset);
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            setCurrentItem(position);
+        }
+    };
 
     public TabSelectorLayout(Context context) {
         super(context);
@@ -30,19 +57,95 @@ public class TabSelectorLayout extends ViewGroup {
 
     public TabSelectorLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
+        TypedArray array = null;
+        try {
+            array = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.TabSelectorLayout, defStyleAttr, 0);
+            int count = array.getIndexCount();
+            for (int i = 0; i < count; i++) {
+                final int index = array.getIndex(i);
+                if (index == R.styleable.TabSelectorLayout_normalTextColor) {
+                    normalTextColor = array.getColor(index, Color.BLACK);
+                } else if (index == R.styleable.TabSelectorLayout_selectTextColor) {
+                    selectTextColor = array.getColor(index, Color.BLACK);
+                } else if (index == R.styleable.TabSelectorLayout_drawablePadding) {
+                    drawablePadding = array.getDimensionPixelSize(index, 10);
+                } else if (index == R.styleable.TabSelectorLayout_android_textSize) {
+                    textSize = array.getDimensionPixelSize(index,
+                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSize, getResources().getDisplayMetrics()));
+                }
+            }
 
-    public void addTab(Tab tab) {
-        if (tab != null) {
-            TabView tabView = new TabView(getContext());
-            tab.tabView = tabView;
-            tab.tabView.setBackgroundDrawable(tab.drawable);
-            addView(tabView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            Log.d(TAG, "normalTextColor = " + normalTextColor + " drawablePadding = " + drawablePadding + " textSize = " + textSize);
+        } finally {
+            if (array != null) {
+                array.recycle();
+            }
         }
     }
 
     public static Tab newTab() {
         return new Tab();
+    }
+
+    public int addTab(final Tab tab) {
+        if (tab == null) {
+            throw new NullPointerException("tab is null");
+        }
+        if (tab.title == null || tab.selectDrawable == null || tab.normalDrawable == null) {
+            throw new IllegalArgumentException("some argument is null");
+        }
+        final TabView tabView = new TabView(getContext());
+        tabView.attach(tab);
+        tabView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(tab.getPosition(), false);
+            }
+        });
+        addView(tabView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        tab.setPosition(indexOfChild(tabView));
+        return tab.position;
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (child instanceof TabView) {
+            super.addView(child, index, params);
+        } else {
+            throw new IllegalArgumentException("child is not TabView");
+        }
+    }
+
+    public void bindViewPager(ViewPager pager) {
+        if (pager == null)
+            throw new NullPointerException("pager is null");
+
+        if (viewPager == pager)
+            return;
+
+        if (viewPager != null)
+            viewPager.removeOnPageChangeListener(pageChangeListener);
+
+        PagerAdapter adapter = pager.getAdapter();
+        if (adapter == null)
+            throw new IllegalArgumentException("pager not set adapter");
+        if (adapter.getCount() != getChildCount())
+            throw new IllegalArgumentException("pager count is not equeals tab count");
+
+        pager.addOnPageChangeListener(pageChangeListener);
+
+        viewPager = pager;
+
+        setCurrentItem(viewPager.getCurrentItem());
+    }
+
+    public void setCurrentItem(int item) {
+        final int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            TabView child = (TabView) getChildAt(i);
+            child.setSelected(item == i);
+        }
     }
 
     /**
@@ -51,20 +154,15 @@ public class TabSelectorLayout extends ViewGroup {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int count = getChildCount();
-
-        Log.d(TAG, "count = " + count);
 
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-
         int height = MeasureSpec.getSize(heightMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-        Log.d(TAG, "widthMode = " + widthMode + " width = " + width + " heightMode = " + heightMode + " height = " + height);
+        final int count = getChildCount();
 
-        int childWidth = width - getPaddingLeft() - getPaddingRight() / count;
+        int childWidth = (width - getPaddingLeft() - getPaddingRight()) / count;
         int maxChildHeight = 0;
+
         for (int i = 0; i < count; i++) {
             final View child = getChildAt(i);
             if (child.getVisibility() != GONE) {
@@ -72,11 +170,9 @@ public class TabSelectorLayout extends ViewGroup {
                 int h = child.getMeasuredHeight();
                 int w = child.getMeasuredWidth();
                 maxChildHeight = maxChildHeight > h ? maxChildHeight : h;
-                Log.d(TAG, "child(" + i + ") w = " + w + " h = " + h);
             }
         }
         int actionBarHeight = getActionBarHeight();
-
         int h = actionBarHeight > maxChildHeight ? actionBarHeight : maxChildHeight;
 
         setMeasuredDimension(width, h + getPaddingTop() + getPaddingBottom());
@@ -95,29 +191,78 @@ public class TabSelectorLayout extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         final int count = getChildCount();
+        int lefPos = getPaddingLeft();
+
+        final int parentTop = getPaddingTop();
+        final int parentBottom = bottom - top - getPaddingBottom();
+        final int parentHeight = parentBottom - parentTop;
+
+        final Rect childRect = new Rect();
+
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+                final int width = child.getMeasuredWidth();
+                final int height = child.getMeasuredHeight();
+
+                lefPos += lp.leftMargin;
+
+                childRect.left = lefPos;
+                childRect.top = (parentHeight - height) / 2;
+                childRect.bottom = childRect.top + height;
+                childRect.right = childRect.left + width;
+
+                // Use the child's gravity and size to determine its final
+                // frame within its container.
+                //Gravity.apply(lp.gravity, width, height, mTmpContainerRect, mTmpChildRect);
+
+                child.layout(childRect.left, childRect.top, childRect.right, childRect.bottom);
+
+                lefPos += width;
+                lefPos += lp.rightMargin;
+            }
+        }
+    }
+
+    /**
+     * Custom per-child layout information.
+     */
+    public static class LayoutParams extends MarginLayoutParams {
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
     }
 
     public static final class Tab {
-        private StateListDrawable drawable;
-        private String title;
-        private int position;
-        TabView tabView;
+        Drawable normalDrawable;
+        Drawable selectDrawable;
+        String title;
+        int position;
 
         private Tab() {
-
         }
 
-        public int getPosition() {
-            return position;
-        }
-
-        public Tab setPosition(int position) {
+        Tab setPosition(int position) {
             this.position = position;
             return this;
         }
 
-        public String getTitle() {
-            return title;
+        public int getPosition() {
+            return position;
         }
 
         public Tab setTitle(String title) {
@@ -125,32 +270,25 @@ public class TabSelectorLayout extends ViewGroup {
             return this;
         }
 
-        public Drawable getDrawable() {
-            return drawable;
+        public Tab setNormalDrawable(Drawable d) {
+            normalDrawable = d;
+            return this;
         }
 
-        public Tab setDrawable(Drawable d) {
-            if (d instanceof StateListDrawable) {
-                this.drawable = (StateListDrawable) d;
-            }
+        public Tab setSelectDrawable(Drawable d) {
+            selectDrawable = d;
             return this;
         }
     }
 
     private class TabView extends View {
-        private int textSize = 12;
-        private int textNormalColor;
-        private int textSelectColor;
+        private int normalAlpha = 255;
         private int viewWidth;
         private int viewHeight;
-        private String text = "ABCD";
-        private final Paint textNormalPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        private final Paint textSelectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        private Bitmap bitmapNormal;
-        private Bitmap bitmapSelect;
-        private final Paint bitmapNormalPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
-        private final Paint bitmapSelectPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+        private final Paint textNormalPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.SUBPIXEL_TEXT_FLAG);
+        private final Paint textSelectPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.SUBPIXEL_TEXT_FLAG);
         private final Rect boundText = new Rect();
+        private Tab tab;
 
         public TabView(Context context) {
             this(context, null);
@@ -163,27 +301,33 @@ public class TabSelectorLayout extends ViewGroup {
         public TabView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
             super(context, attrs, defStyleAttr);
             initText();
-            initView();
         }
 
         private void initText() {
-            textNormalPaint.setColor(textNormalColor);
-            textNormalPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSize, getResources().getDisplayMetrics()));
-            textNormalPaint.setAlpha(255);
+            textNormalPaint.setColor(normalTextColor);
+            textNormalPaint.setTextSize(textSize);
 
-            textSelectPaint.setColor(textSelectColor);
-            textSelectPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSize, getResources().getDisplayMetrics()));
-            textSelectPaint.setAlpha(0);
-
-            bitmapNormalPaint.setAlpha(255);
-            bitmapSelectPaint.setAlpha(0);
-        }
-
-        private void initView() {
+            textSelectPaint.setColor(selectTextColor);
+            textSelectPaint.setTextSize(textSize);
         }
 
         private void measureText() {
-            textNormalPaint.getTextBounds(text, 0, text.length(), boundText);
+            textNormalPaint.getTextBounds(tab.title, 0, tab.title.length(), boundText);
+        }
+
+        void attach(Tab tab) {
+            this.tab = tab;
+        }
+
+        void setTabOffSet(float offSet) {
+            normalAlpha = (int) (255 - offSet * 255);
+            invalidate();
+        }
+
+        @Override
+        public void setSelected(boolean selected) {
+            normalAlpha = selected ? 0 : 255;
+            super.setSelected(selected);
         }
 
         @Override
@@ -196,8 +340,8 @@ public class TabSelectorLayout extends ViewGroup {
             int w = 0, h = 0;
 
             measureText();
-            Log.d(TAG, "child widthMode = " + widthMode + " width = " + width + " heightMode = " + heightMode + " height = " + height);
-            int contentWidth = Math.max(boundText.width(), getBackground().getIntrinsicWidth());
+
+            int contentWidth = Math.max(boundText.width(), Math.max(tab.normalDrawable.getIntrinsicWidth(), tab.selectDrawable.getIntrinsicWidth()));
             int desiredWidth = getPaddingLeft() + getPaddingRight() + contentWidth;
             switch (widthMode) {
                 case MeasureSpec.AT_MOST:
@@ -210,7 +354,7 @@ public class TabSelectorLayout extends ViewGroup {
                     w = desiredWidth;
                     break;
             }
-            int contentHeight = boundText.height() + getBackground().getIntrinsicHeight();
+            int contentHeight = (int) (textNormalPaint.getFontSpacing() + Math.max(tab.normalDrawable.getIntrinsicHeight(), tab.selectDrawable.getIntrinsicHeight()) + drawablePadding);
             int desiredHeight = getPaddingTop() + getPaddingBottom() + contentHeight;
             switch (heightMode) {
                 case MeasureSpec.AT_MOST:
@@ -220,7 +364,7 @@ public class TabSelectorLayout extends ViewGroup {
                     h = height;
                     break;
                 case MeasureSpec.UNSPECIFIED:
-                    h = contentHeight;
+                    h = height;
                     break;
             }
             setMeasuredDimension(w, h);
@@ -231,6 +375,39 @@ public class TabSelectorLayout extends ViewGroup {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
+            drawNoramalBitmap(canvas);
+            drawSelectBitmap(canvas);
+            drawText(canvas);
+        }
+
+        private void drawText(Canvas canvas) {
+            int drawableHeight = Math.max(tab.normalDrawable.getIntrinsicHeight(), tab.selectDrawable.getIntrinsicHeight());
+            float x = (viewWidth - boundText.width()) / 2.0f;
+            float y = (viewHeight + drawableHeight + boundText.height() + drawablePadding) >> 1;
+            textNormalPaint.setAlpha(normalAlpha);
+            canvas.drawText(tab.title, x, y, textNormalPaint);
+            textSelectPaint.setAlpha(255 - normalAlpha);
+            canvas.drawText(tab.title, x, y, textSelectPaint);
+        }
+
+        private void drawNoramalBitmap(Canvas canvas) {
+            int width = tab.normalDrawable.getIntrinsicWidth();
+            int height = tab.normalDrawable.getIntrinsicHeight();
+            int left = (viewWidth - width) / 2;
+            int top = (viewHeight - height - boundText.height() - drawablePadding) >> 1;
+            tab.normalDrawable.setBounds(left, top, left + width, top + height);
+            tab.normalDrawable.setAlpha(normalAlpha);
+            tab.normalDrawable.draw(canvas);
+        }
+
+        private void drawSelectBitmap(Canvas canvas) {
+            int width = tab.selectDrawable.getIntrinsicWidth();
+            int height = tab.selectDrawable.getIntrinsicHeight();
+            int left = (viewWidth - width) / 2;
+            int top = (viewHeight - height - boundText.height() - drawablePadding) >> 1;
+            tab.selectDrawable.setBounds(left, top, left + width, top + height);
+            tab.selectDrawable.setAlpha(255 - normalAlpha);
+            tab.selectDrawable.draw(canvas);
         }
     }
 
